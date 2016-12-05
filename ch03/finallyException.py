d7286c5e2fbbdec878465e06bff6059a0d062ed9
jz3707 <ans3707@126.com>
2016-12-05 23:33:57 +0800

    FinallyTest

--- ch03/finallyException.py
+++ ch03/finallyException.py
@@ -7,6 +7,17 @@
 def FinallyTest():
     """
 
+    try中raise IndexError之后except没能捕获这个exception
+    系统会将这个exception临时保存起来
+    当finally执行结束的时候，临时保存起来的exception会再次被跑出
+    但是如果finally中有return，break，那么临时保存的exception就会丢失
+    从而导致异常屏蔽
+
+    所以这个function的结果是：
+        i am starting....
+        i sm starting....
+        finally executed
+
     :return:
     """
     print 'i am starting....'
@@ -17,7 +28,7 @@ def FinallyTest():
             print 'i am starting....'
             raise IndexError('r')
 
-        except NameError, e:
+        except NameError as e:
             print 'NameError happened %s', e
 
         finally:
@@ -26,6 +37,26 @@ def FinallyTest():
 
 
 def ReturnTest(a):
+
+    """
+
+    a = 0,
+    try跑出异常，except中捕获了这个ValueError，并处理
+    然后执行finally section。
+
+    a = 2
+    try 进入else模块，在return a之前进入finnaly section，
+    并return -1
+
+    所以结果：
+        -1
+        -1
+
+
+
+    :param a:
+    :return:
+    """
     try:
         if a <= 0:
             raise ValueError('value error')
